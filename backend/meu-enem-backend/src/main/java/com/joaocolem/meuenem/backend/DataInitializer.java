fe6b7f855bd5dc99726a2fb59fb882b01b6385d2
JoaoColem <joaocostaval@gmail.com>
2025-02-26 08:37:49 -0300

    Refactor data initialization and security configuration for improved readability and environment variable support

--- backend/meu-enem-backend/src/main/java/com/joaocolem/meuenem/backend/DataInitializer.java
+++ backend/meu-enem-backend/src/main/java/com/joaocolem/meuenem/backend/DataInitializer.java
@@ -20,12 +20,11 @@ public class DataInitializer implements CommandLineRunner {
     public DataInitializer(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
-        this.passwordEncoder = new BCryptPasswordEncoder(); // Password encryption
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @Override
     public void run(String... args) throws Exception {
-        // Check if the ROLE_ADMIN role already exists before creating it
         Role adminRole = roleRepository.findByName("ROLE_ADMIN");
         if (adminRole == null) {
             adminRole = new Role();
@@ -33,7 +32,6 @@ public class DataInitializer implements CommandLineRunner {
             roleRepository.save(adminRole);
         }
 
-        // Check if the ROLE_USER role already exists before creating it
         Role userRole = roleRepository.findByName("ROLE_USER");
         if (userRole == null) {
             userRole = new Role();
@@ -41,23 +39,21 @@ public class DataInitializer implements CommandLineRunner {
             roleRepository.save(userRole);
         }
 
-        // Check if the admin user already exists before creating it
         if (userRepository.findByEmail("admin@example.com").isEmpty()) {
             User admin = new User();
             admin.setName("Admin");
             admin.setEmail("admin@example.com");
-            admin.setPassword(passwordEncoder.encode("admin123")); // Encrypt the password
-            admin.setRoles(Set.of(adminRole, userRole)); // Assign roles to the admin
+            admin.setPassword(passwordEncoder.encode("admin123"));
+            admin.setRoles(Set.of(adminRole, userRole));
             userRepository.save(admin);
         }
 
-        // Check if the regular user already exists before creating it
         if (userRepository.findByEmail("user@example.com").isEmpty()) {
             User user = new User();
             user.setName("User");
             user.setEmail("user@example.com");
-            user.setPassword(passwordEncoder.encode("user123")); // Encrypt the password
-            user.setRoles(Set.of(userRole)); // Assign the ROLE_USER to the user
+            user.setPassword(passwordEncoder.encode("user123"));
+            user.setRoles(Set.of(userRole));
             userRepository.save(user);
         }
     }
